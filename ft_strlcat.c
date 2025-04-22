cf16fb8da72af1d31a8ced3588a9a833743b14bb
Tomas Andrade <toandrad@c2r8s1.42lisboa.com>
2025-04-22 13:25:31 +0100

    Finished libft

---
+++ ft_strlcat.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: toandrad <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/14 11:55:52 by toandrad          #+#    #+#             */
+/*   Updated: 2025/04/14 12:09:26 by toandrad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t	dstsize)
+{
+	size_t	dst_len;
+	size_t	src_len;
+	size_t	i;
+	size_t	j;
+
+	dst_len = 0;
+	while (dst[dst_len] != '\0' && dst_len < dstsize)
+		dst_len++;
+	src_len = 0;
+	while (src[src_len] != '\0')
+		src_len++;
+	if (dst_len == dstsize)
+		return (dstsize + src_len);
+	i = 0;
+	j = dst_len;
+	while (src[i] != '\0' && j < (dstsize - 1))
+	{
+		dst[j] = src[i];
+		i++;
+		j++;
+	}
+	dst[j] = '\0';
+	return (dst_len + src_len);
+}
---
+++ ft_strlcpy.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: toandrad <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/14 11:19:00 by toandrad          #+#    #+#             */
+/*   Updated: 2025/04/14 12:09:58 by toandrad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	src_len;
+	size_t	i;
+
+	src_len = 0;
+	while (src[src_len] != '\0')
+		src_len++;
+	if (dstsize == 0)
+		return (src_len);
+	i = 0;
+	while (src[i] != '\0' && i < (dstsize - 1))
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (src_len);
+}
